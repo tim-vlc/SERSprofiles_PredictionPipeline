6cd603b3d2da750918f0a1fa1bb58029b119c9fa
tim-vlc <timvalencony@instance-5.us-central1-a.c.infra-mix-393221.internal>
2023-08-10 22:33:35 +0000

    modify cnn_class

--- NNs/cnn_class.py
+++ NNs/cnn_class.py
@@ -8,6 +8,9 @@ import torch.optim as optim
 from cnn import CNN
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+import matplotlib.pyplot as plt
 
 type_ = 'processed'
 input_size = 1650 if type_ == 'raw' else 851
@@ -17,14 +20,17 @@ ratio = 0.8
 #test_data = pd.read_csv(f'../../CSVs/processed_data/test_data.csv') if type_ == 'augmented' else pd.read_csv(f'../../CSVs/{type_}_data/{ratio}complete_test_data.csv')
 #test_data = pd.read_csv(f'../../CSVs/processed_data/{ratio}complete_test_data.csv')
 
-data = pd.read_csv('../../complete_processed_data.csv')
-print(len(data))
-data.dropna(inplace=True)
+#data = pd.read_csv('../../complete_processed_data.csv')
+#print(len(data))
+#data.dropna(inplace=True)
 # Randomly select 80% of the data
-train_data = data.sample(frac=ratio, random_state=42)
-print(len(train_data))
-test_data = data.drop(train_data.index)
-test_data.to_csv('../../test_data.csv', index=False)
+#train_data = data.sample(frac=ratio, random_state=42)
+#print(len(train_data))
+#test_data = data.drop(train_data.index)
+#test_data.to_csv('../../test_data.csv', index=False)
+
+train_data = pd.read_csv('../../binary/train_binary.csv').sample(frac=1.).reset_index(drop=True)
+test_data = pd.read_csv('../../binary/test_binary.csv')
 
 #train_data = pd.read_csv(f'../../CSVs/augmented_data/{ratio}gan_train_data.csv').sample(frac=1.).reset_index(drop=True)
 #test_data = pd.read_csv(f'../../CSVs/augmented_data/{ratio}gan_test_data.csv')
@@ -39,15 +45,15 @@ device = torch.device("cuda:0")
 #train_data['labels'] = train_data['labels'].replace(replace_)
 #test_data['labels'] = test_data['labels'].replace(replace_)
 
-X_test, y_test = test_data.iloc[:,:-1], test_data['labels']
-X_train, y_train = train_data.iloc[:,:-1], train_data['labels']
+X_test, y_test = test_data.iloc[:,:-2], test_data['labels']
+X_train, y_train = train_data.iloc[:,:-2], train_data['labels']
 
-output_size = 7
+output_size = 2
 
 dropratio = 0.15
 alpha = 0.0001 # learning rate
 batch = 175
-ep = 20 # epoch
+ep = 70 # epoch
     
 model = CNN(input_size, output_size)
 model.to(device)
@@ -107,16 +113,28 @@ for epoch in range(ep):
 correct = 0
 total = 0
 
+full_truth = np.array([])
+full_outputs = np.array([])
+full_predicted = np.array([])
+
 with torch.no_grad():
     for i in range(0, len(X_test), batch):
         batch_X = X_test[i:i+batch].clone().detach().float().to(device)
         batch_y = y_test[i:i+batch].clone()
         outputs = (model(batch_X)).detach().cpu().numpy()
 
+        if i == 0:
+            full_outputs = outputs
+        else:
+            full_outputs = np.concatenate((full_outputs, outputs))
+
         torch.cuda.empty_cache()
 
         predicted = np.argmax(outputs, 1)
         truth = np.argmax(batch_y, 1).detach().numpy()
+
+        full_predicted = np.concatenate((full_predicted, predicted))
+        full_truth = np.concatenate((full_truth, truth))
 
         total += truth.shape[0]
         correct += (predicted == truth).sum().item()
@@ -124,6 +142,39 @@ with torch.no_grad():
 print('Accuracy of the network on the test data: %f %%' % (
     100 * correct / total))
 
-print('Saving model')
-torch.save(model.state_dict(), f"../saved_models/cnn_model.pth")
-print('Saved.')
+#print('Saving model')
+#torch.save(model.state_dict(), f"../saved_models/cnn_model.pth")
+#print('Saved.')
+
+raw_outputs = []
+prediction_list = []
+labels_list=[]
+
+raw_outputs.append(full_outputs)
+prediction_list.append(full_predicted)
+labels_list.append(full_truth)
+
+# Create a dictionary to map encoded labels to original labels
+label_mapping = dict(zip(full_truth, label_test))
+labels_sorted = dict(sorted(label_mapping.items())).values()
+
+probabilities = np.concatenate([np.asarray((i)) for i in raw_outputs])
+prediction_array = np.concatenate([np.asarray(i) for i in prediction_list])
+labels_array = np.concatenate([np.asarray(i) for i in labels_list])
+
+test_truth = np.argmax(y_test, 1)
+
+labels = list(np.unique(test_truth))
+
+confusion_array = confusion_matrix(labels_array, prediction_array, labels=labels, sample_weight=None, normalize=None)
+
+plt.figure(figsize = (10,7))
+sn.heatmap(confusion_array, annot=True, fmt='d',cmap="OrRd")
+plt.xticks([0.5,1.5],labels=labels_sorted)
+plt.yticks([0.45,1.45],labels=labels_sorted)
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+
+plt.title("Confusion matrix of trained CNN for Pancreatic Cyst exosome SERS profiles")
+
+plt.savefig('confmat.png')
